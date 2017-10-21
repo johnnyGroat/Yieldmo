86baa87f4a406569968e27918e0c54167f20c909
Johnny Groat <johnny@skybuds.com>
2017-10-21 12:34:20 -0400

    implemented tests based on understanding of the question

--- src/SegmentsTest.java
+++ src/SegmentsTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 class SegmentsTest {
 	public ArrayList<Segment> td = new ArrayList<Segment>();
 	
-	Segment td1 = new Segment(1, 8);
-	Segment td2 = new Segment(10, 12);
-	Segment td3 = new Segment(4, 18);
+	Segment s1 = new Segment(1, 8);
+    Segment s2 = new Segment(10, 12);
+    Segment s3 = new Segment(18, 20);
+    Segment s4 = new Segment(5, 7);
+    Segment s5 = new Segment(24,27);
+    Segment s6 = new Segment(29, 33);
 	
-	Segments seg = new Segments();
 	
 	@Test
 	void testSegmentLength() {
@@ -34,6 +36,29 @@ class SegmentsTest {
 		seg.addSegment(td1);
 		
 		assertEquals(seg.getTimeDiffSize(), 0);
+	}
+	
+	@Test
+	void testProblemOutput() {
+		Segments seg = new Segments();
+		
+		seg.addSegment(s1);
+		seg.addSegment(s2);
+		seg.addSegment(s3);
+		seg.addSegment(s4);
+		seg.addSegment(s5);
+		seg.addSegment(s6);
+	    
+		ArrayList<Segment> output = seg.getOuputSegment();
+		
+		Segment segOut1 = output.get(0);
+		Segment segOut2 = output.get(1);
+		
+	    assertEquals(output.size(), 2);
+	    assertEquals(segOut1.getStartTime(), 1);
+	    assertEquals(segOut1.getEndTime(), 12); 
+	    assertEquals(segOut2.getStartTime(), 18);
+	    assertEquals(segOut2.getEndTime(), 33);
 	}
 
 }
